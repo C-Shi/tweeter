b1f2dd4b757aa8e56717c4ce7fcbb53c879ce35a
C-Shi <cshi@ualberta.ca>
2018-09-11 15:28:34 -0600

    ajax fetching data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,23 +6,26 @@
 
 // calling renderTweets function which exists in render-tweets.js file
 $(document).ready(function(){
-  renderTweets(data);
+  
   
   // ajax call
   const $form = $(".new-tweet form");
   $form.submit(function(event){
     // prevent default event on submitting
     event.preventDefault();
+    // convert form data to query string
     const data = $(this).serialize();
     //
     $.ajax("/tweets/",{
       // in the object, specify the method and what data to send
       method: 'POST',
       data: data,
-    }).done(function(tweet){
-      console.log(tweet);
+    }).done(function(){
+      loadTweets(); // this is not good b/c we are re-rendering the entire database, will replace in the future
     })
-  })
+  });
+
+  loadTweets(); // on document ready, load all tweets from database
 
 }) // end of document ready func
 
